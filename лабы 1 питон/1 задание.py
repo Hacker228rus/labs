5fc5a9bc1abd3f96a371889d340340e0bdad31b7
Hacker228rus <aleksandrsaltykov80@gmail.com>
2024-10-20 15:34:39 +0300

    Update 1 задание.py
    Изменены названия переменных, поставлены пробелы вокруг математических операторов

--- лабы 1 питон/1 задание.py
+++ лабы 1 питон/1 задание.py
@@ -1,8 +1,8 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 summ = sum(numbers[:4] + numbers[5:])
-dlina = len(numbers)
-srednee = summ/dlina
-numbers[numbers.index(None)] = srednee
+lenght = len(numbers)
+average = summ / lenght
+numbers[numbers.index(None)] = average
 # TODO заменить значение пропущенного элемента средним арифметическим
 
 print("Измененный список:", numbers)
