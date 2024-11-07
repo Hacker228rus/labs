fa9d9b7f66c472918daac60301146be8fdbc5161
Hacker228rus <aleksandrsaltykov80@gmail.com>
2024-11-08 02:26:38 +0300

    Лабораторная работа 3 python

---
+++ 1 задание.py
@@ -0,0 +1,14 @@
+def find_first_occurrence(products, target):
+    for index, product in enumerate(products):
+        if product == target:
+            return index
+    return None     # TODO Напишите функцию для поиска индекса товара
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find_first_occurrence(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
