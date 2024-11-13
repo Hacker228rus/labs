dd9d47f9bf2a8f589e7376b24fb16e3c4ac4aa10
Hacker228rus <aleksandrsaltykov80@gmail.com>
2024-11-13 17:01:19 +0300

    Update 2 задание.py

--- 2 задание.py
+++ 2 задание.py
@@ -1,16 +1,14 @@
 import json
-
 import csv
-
 
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
 
 def task() -> None:
-    """Десериализация файла input.csv"""
+    """""Процесс преобразования файла input.csv"""
     with open(INPUT_FILENAME, 'r') as csv_file:
-        dictionary = [line for line in csv.DictReader(csv_file)]
+        dictionary = [row for row in csv.DictReader(csv_file)]
 
     """Сериализация файла в output.json"""
     with open(OUTPUT_FILENAME, 'w') as json_file:
