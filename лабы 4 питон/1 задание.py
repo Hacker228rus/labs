9933bf04289010ba9505048b185de5b307f56849
Hacker228rus <aleksandrsaltykov80@gmail.com>
2024-11-13 16:51:37 +0300

    лабораторная 4

---
+++ лабы 4 питон/1 задание.py
@@ -0,0 +1,15 @@
+import json
+
+def task() -> float:
+    """открытие файла input.json"""
+    with open('input.json') as f:
+        data = json.load(f)
+        total_sum = 0
+        for line in data:
+            multiplier = line['score'] * line['weight']
+            total_sum += multiplier
+    round_summ = round(total_sum, 3)
+    return round_summ
+
+
+print(task())
---
+++ лабы 4 питон/2 задание.py
@@ -0,0 +1,24 @@
+import json
+import csv
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    """""Процесс преобразования файла input.csv"""
+    with open(INPUT_FILENAME, 'r') as csv_file:
+        dictionary = [row for row in csv.DictReader(csv_file)]
+
+    """Сериализация файла в output.json"""
+    with open(OUTPUT_FILENAME, 'w') as json_file:
+        json.dump(dictionary, json_file, indent=4)
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
